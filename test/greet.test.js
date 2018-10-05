791ad7946628b0116375d371c1bc0cb1193103e2
Londiwe <celelondiwe3@gmail.com>
2018-10-05 11:59:26 +0200

    building travis bagde

--- test/greet.test.js
+++ test/greet.test.js
@@ -3,7 +3,7 @@ const greetings = require('../greetings');
 const pg = require('pg');
 const Pool = pg.Pool;
 
-const connectionString = process.env.DATABASE_URL || 'postgresql://coder:pg123@localhost:5432/greetings';
+const connectionString = process.env.DATABASE_URL || 'postgresql://postgres@localhost:5432/greetings';
 
 const pool = new Pool({
     connectionString
